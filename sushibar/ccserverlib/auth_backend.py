7b8f8623e7ad64e48deb3d114b2dd34b935513bf
Ivan Savov <ivan.savov@gmail.com>
2017-07-11 11:44:48 -0400

    Library for talking to Content Curation server

---
+++ sushibar/ccserverlib/auth_backend.py
@@ -0,0 +1,50 @@
+
+from sushibar.users.models import BarUser
+
+from .services import ccserver_authenticate_user
+
+
+
+class ContentCurationServerAuthBackend(object):
+    """
+    This authentication backend uses the content curation server token (`cctoken`)
+    instead of a password.
+
+    If the `cctoken` is valid, a new BarUser will be created and returned.
+    """
+    def authenticate(self, request, username=None, password=None):
+        print('in custom CCServer-backed authenticate method...')
+        ccemail = username  # signin form's username field is actually an email
+        cctoken = password  # signin form's password field is actuall the cctoken
+
+        # Check if BarUser with this `ccemail` and `cctoken` already exists:
+        try:
+            existing_baruser = BarUser.objects.get(email=ccemail, cctoken=cctoken)
+            return existing_baruser
+
+        except BarUser.DoesNotExist:
+            # No BarUser exists with this cctoken, let's check if token is valid
+            status, email_or_msg = ccserver_authenticate_user(cctoken, ccemail)
+            if status == 'success':
+                # We know `cctoken` and `ccemail` provided are valid, create the BarUser
+                new_baruser = BarUser.objects.create(
+                    username=ccemail,
+                    email=ccemail,
+                    cctoken=cctoken,
+                    is_staff = ('@learningequality.org' in ccemail),
+                )
+                return new_baruser
+
+            elif status == 'failure':
+                print('Authentication failed:', email_or_msg)
+                return None
+
+            else:
+                raise ValueError('Unrecognized status from ccserver_authenticate_user')
+
+
+    def get_user(self, user_id):
+        try:
+            return BarUser.objects.get(pk=user_id)
+        except BarUser.DoesNotExist:
+            return None
