eccd6cddd2dde2e4a180a9a7237e4c9431b494f6
Ivan Savov <ivan.savov@gmail.com>
2018-04-06 13:51:28 -0400

    remove unnecessary line

--- sushibar/__init__.py
+++ sushibar/__init__.py
@@ -8,4 +8,3 @@ __version_info__ = tuple([int(num) if num.isdigit() else num for num in __versio
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
-# default_app_config = "sushibar.apps.ContentConfig"   # Apr 6: needed???
